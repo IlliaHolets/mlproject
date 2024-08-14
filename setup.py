3c3a2f0bbfcb97a6bfb773d0553ca6de1f96d017
IlliaHolets <illia.holets@gmail.com>
2024-08-14 10:36:37 +0300

    EDA and Problem statement

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name = 'mlproject',
     version = '0.0.1',
-    author = 'Illia',
+    author = 'Illia (by tutorial from Krish Naik)',
     author_email = 'illia.holets@gmail.com',
     packages = find_packages(),
     install_requires = get_requirements('requirements.txt')
